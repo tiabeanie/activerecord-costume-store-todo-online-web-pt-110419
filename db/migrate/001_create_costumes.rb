bf56e631803d95d046e2673df90a660a6031674e
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-02-19 20:27:20 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,3 +8,14 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 
+class CreateCostumes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
+end
